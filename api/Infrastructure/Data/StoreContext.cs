ddc366b01af394cda6d6f9f9899618775f66c8ec
SinanZzz <ergun.sinan31@gmail.com>
2024-08-07 18:12:39 +0200

    Création Projet API

---
+++ api/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class StoreContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public StoreContext(DbContextOptions options) : base(options)
+    { 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
+}
